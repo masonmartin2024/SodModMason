e73ed4e71627e7f02500c671ef82b33a4e04a035
masonmartin2024 <masonmartin2024@fau.edu>
2025-11-17 21:02:05 -0500

    Update params.py

--- sodmod/params.py
+++ sodmod/params.py
@@ -65,9 +65,9 @@ def params(cond = 'WT37', I_scale = 1, typ = 'IN', paradigm = 'step', I_off = 0,
     p['Egaba'] = -80           # GABA reversal potential (Desthexe et al 1994)
     p['aGABA'] = 0.53          # ms^(-1) mM^(-1)
     p['bGABA'] = 0.184         # ms^(-1)
-    p['gGABA'] = 10             # µS
-    p['GABAamp'] = 5           # mM
-    p['GABAdur'] = 5           # ms
+    p['gGABA'] = 1             # µS
+    p['GABAamp'] = 1           # mM
+    p['GABAdur'] = 1           # ms
 
 
     # Type-specific parameters
