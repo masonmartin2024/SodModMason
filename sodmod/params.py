c854ce16a68ab8a540cde8c931b8cc6af0ff730e
masonmartin2024 <masonmartin2024@fau.edu>
2025-11-17 23:24:48 -0500

    Update params.py

--- sodmod/params.py
+++ sodmod/params.py
@@ -63,7 +63,7 @@ def params(cond = 'WT37', I_scale = 1, typ = 'IN', paradigm = 'step', I_off = 0,
     # Synaptic parameters (GABA-A transmitter)
     #-------------------------------------------------------------------------------
     p['Egaba'] = -80           # GABA reversal potential (Desthexe et al 1994)
-    p['aGABA'] = 0.53          # ms^(-1) mM^(-1)
+    p['aGABA'] = 5          # ms^(-1) mM^(-1)
     p['bGABA'] = 0.184         # ms^(-1)
     p['gGABA'] = 1             # µS
     p['GABAamp'] = 1           # mM
